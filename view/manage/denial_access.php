30180ff5b5aa944b28547293465077b248df997c
Solation <glglwjswls3@naver.com>
2018-08-16 15:03:10 +0900

    Revert "initial commit"
    This reverts commit 64d4fa739016b05e9706799115fc81cda5616b0b.

--- view/manage/denial_access.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  //  header("Content-Type: text/html; charset=UTF-8");
-    echo "<script>alert('로그인을 하셔야 합니다.');";
-    echo "window.location.replace('../account/login.php');</script>";
-    exit;
-?>
